bb38bfff50f917dc614db1e813ec7a5aac8fe7d9
Gustavo Araujo <gustavoaraujomoraes.2005@gmail.com>
2026-05-10 18:23:18 -0300

    fix: deploy final com nitro preset

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BmyppIOp.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/NITRO5/Downloads/Evolvy-main/Evolvy-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-rALspq7I.js"] }, "/": { filePath: "C:/Users/NITRO5/Downloads/Evolvy-main/Evolvy-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DGBcM-Dd.js"] } }, clientEntry: "/assets/index-rALspq7I.js" });
+export {
+  tsrStartManifest
+};
